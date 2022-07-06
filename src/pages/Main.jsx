a32d563765c98441a81c8cee28fa4ccb6a003069
GiveMePevko <anttt501@gmail.com>
2022-07-06 12:25:53 +0700

    Revert "create subbar"
    This reverts commit b784292dcdb0c5db0be4842ef04718c2ddbff9ec.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -19,44 +19,6 @@ const Main = () => {
 
   return (
     <Grid container justifyContent="center" alignItems="center">
-      <div className="navigation">
-        <ul>
-          <li className="navigation-list active">
-            <b></b>
-            <b></b>
-            <a href="#">
-              <span className="icon">
-                <ion-icon name="briefcase-outline"></ion-icon>
-              </span>
-              <span className="title">Tasks</span>
-            </a>
-          </li>
-          <li className="navigation-list">
-            <b></b>
-            <b></b>
-            <a href="#">
-              <span className="icon">
-                <ion-icon name="id-card-outline"></ion-icon>
-              </span>
-              <span className="title">Reports</span>
-            </a>
-          </li>
-          <li className="navigation-list">
-            <b></b>
-            <b></b>
-            <a href="#">
-              <span className="icon">
-                <ion-icon name="people-outline"></ion-icon>
-              </span>
-              <span className="title">Team</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="toggle">
-        <ion-icon name="menu-outline" className="open"></ion-icon>
-        <ion-icon name="close-outline" className="close"></ion-icon>
-      </div>
       <Grid item textAlign="center">
         <Heading>Tempus</Heading>
 
